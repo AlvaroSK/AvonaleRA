4af3557d45088eb1f0817d363a2e12ab51d0c0b8
José Alvaro <alvaro.pepper@gmail.com>
2022-04-06 17:13:04 -0300

    Adição do DBContext no Startup

--- AvonaleRA/Startup.cs
+++ AvonaleRA/Startup.cs
@@ -1,7 +1,9 @@
+using AvonaleRA.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,7 @@ namespace AvonaleRA
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AvonaleRA", Version = "v1" });
             });
+            services.AddDbContext<AppDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("ServerConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
