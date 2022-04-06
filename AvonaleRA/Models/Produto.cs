c79d2b4b01a434c3dc4fe298ed5bef4142d45900
José Alvaro <alvaro.pepper@gmail.com>
2022-04-06 16:43:29 -0300

    Criação da classe Produto

---
+++ AvonaleRA/Models/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace AvonaleRA.Models
+{
+    public class Produto
+    {
+
+        public int Id { get; set; }
+        public string nome { get; set; }
+        public decimal valor_unitario { get; set; }
+        public int qtde_estoque { get; set; }
+    }
+}
