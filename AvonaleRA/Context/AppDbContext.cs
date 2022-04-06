fc7ab130bf3f6a8a1b127632142f9014cc9fad1a
José Alvaro <alvaro.pepper@gmail.com>
2022-04-06 17:02:43 -0300

    Criação do Context

---
+++ AvonaleRA/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AvonaleRA.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AvonaleRA.Context
+{
+    public class AppDbContext : DbContext 
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\\mssqllocaldb;Database=AvonaleRA;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
