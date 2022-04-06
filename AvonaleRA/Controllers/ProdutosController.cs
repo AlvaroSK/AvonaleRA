a1de12ec08f4b73a515236e27678cd19b0d319f8
José Alvaro <alvaro.pepper@gmail.com>
2022-04-06 18:20:32 -0300

    Criação do controller dos produtos

---
+++ AvonaleRA/Controllers/ProdutosController.cs
@@ -0,0 +1,30 @@
+﻿using AvonaleRA.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace AvonaleRA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public ProdutosController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProdutos()
+        {
+            return Ok(
+                (new
+                {
+                    success = true,
+                    data = await _appDbContext.Produtos.ToListAsync()
+                }));
+        }
+    }
+}
